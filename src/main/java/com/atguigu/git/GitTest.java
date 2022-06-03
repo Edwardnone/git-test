d6491f75b22fbdec382dfcec0e8e82cd80212cb6
yangle <yangle_edward@163.com>
2022-06-03 15:54:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author yangle
+ * @Description test
+ * @Date 2022/6/3 15:28
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
